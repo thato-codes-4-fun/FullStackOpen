5c0f48fc89e1d9d9b227ccedb3cb2bb334f57716
Thato Lebethe <thatofreelance@gmail.com>
2023-05-23 21:56:20 +0200

    unicaf step 1 and 2

--- Part1/unicaf/unicaf/src/App.js
+++ Part1/unicaf/unicaf/src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
-function App() {
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [total , setTotal] = useState(0)
+
+  const handleGood = () => {
+    setGood(good+1);
+    setTotal(total+1)
+
+  }
+
+  const handleNuetral = () => {
+    setNeutral(neutral+1);
+    setTotal(total+1)
+  }
+
+  const handleBad = () => {
+    setBad(bad+1);
+    setTotal(total+1)
+  }
+
+  const getPositive=()=> {
+    if (total === 0){
+      return 0;
+    }
+    return (good/total)*100;
+  }
+
+  const getAverage = ()=> {
+    if (total === 0) {
+      return 0;
+    }
+    return (good - bad) / total
+  }
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Button handlepress={handleGood} string={'good'}/>
+      <Button handlepress={handleNuetral} string={'neutral'} />
+      <Button handlepress={handleBad} string={'bad'}/>
+      <h2>Statistics</h2>
+
+      <p>Good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+
+      <p>all {total}</p>
+      <p>average {getAverage()}</p>
+      <p>positive {getPositive()}  %</p>
     </div>
-  );
+  )
 }
 
-export default App;
+const Header = ()=> <h1>Give Feedback</h1>
+
+const Button = ({handlepress, string})=> {
+  return <button onClick={handlepress}>{string}</button>
+}
+
+
+
+export default App
+
