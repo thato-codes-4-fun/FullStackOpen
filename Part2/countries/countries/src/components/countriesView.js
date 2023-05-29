3746ced4fcc8f8d026d0caf7b2e070a9ea9e08b2
Thato Lebethe <thatofreelance@gmail.com>
2023-05-29 17:10:15 +0200

    step 1 data for countries

--- Part2/countries/countries/src/components/countriesView.js
+++ Part2/countries/countries/src/components/countriesView.js
@@ -1,15 +1,33 @@
 
 const ViewCountries = ({countryList, search,})=> {
     if (search === ''){
-      return (
-        <>
-          <h2>Country List</h2>
-          no data
-        </>
-      )
+      return 
     }
     else {
       const filteredCountry = countryList.filter(country => country['name']['common'].toLowerCase().includes(search))
+      if(filteredCountry.length > 10){
+        return (
+        <>
+            <h1>Countries list</h1>
+            <p>Too many matches, specify another field</p>
+        </>
+        )
+      }
+      else if (filteredCountry.length === 1){
+        const extractCountry = filteredCountry[0]
+        return (
+            <>
+                <h1>{extractCountry['name']['common']}</h1>
+                <p>capital {extractCountry['capital'][0]}</p>
+                <p>area {extractCountry['area']}</p>
+                <h2>languages</h2>
+                <ul>
+                    <LanguageList languageObj={extractCountry['languages']}/>
+                </ul>
+                <p style={{fontSize: 100}}>{extractCountry['flag']}</p>
+            </>
+        )
+      }
       return (
         <>
           <h2>Country List</h2>
@@ -20,6 +38,16 @@ const ViewCountries = ({countryList, search,})=> {
       )
     }
   }
+
+const LanguageList = ({languageObj})=> {
+    let languages = []
+    for (let langs in languageObj){
+        languages.push(languageObj[langs])
+    }
+    return (
+        languages.map(lang=> <li key={lang}>{lang}</li>)
+    )
+}
 
 
 export default ViewCountries
