0e389e7496301eee01d28155e7c1b6cc83988153
Thato Lebethe <thatofreelance@gmail.com>
2023-05-29 15:28:59 +0200

    countries api call and filter by search

--- Part2/countries/countries/src/App.js
+++ Part2/countries/countries/src/App.js
@@ -1,22 +1,40 @@
-import logo from './logo.svg';
+
 import './App.css';
-import { useState } from 'react';
+import { useState , useEffect} from 'react';
+import axios from 'axios';
 
 function App() {
   const [ search, setSearch ] = useState('')
+  const [ countryList, setCountryList ] = useState([])
 
 
   const handleSearch =(event)=> {
     setSearch(event.target.value)
   }
 
+  const getCountries = ()=> {
+    const baseUrl = 'https://studies.cs.helsinki.fi/restcountries/api/all'
+    axios
+    .get(baseUrl)
+    .then(country => country)
+    .then(res => {
+      let countries = res.data
+      setCountryList(countries)
+    })
+    .catch(e=>console.log('error fetching data!!', e.message))
+  }
+
+  useEffect(getCountries, [])
+
 
   return (
     <div className='container'>
      <h1>Countries</h1>
      <SearchBar search={search} handleSearch={handleSearch}/>
-      <ViewCountries handleSearch={handleSearch} search={search}/>
-     
+      <ViewCountries 
+        countryList={countryList} 
+        search={search}
+      />
     </div>
   );
 }
@@ -33,12 +51,26 @@ const SearchBar = ({search, handleSearch})=> {
   )
 }
 
-const ViewCountries = ()=> {
-  return (
-    <>
-      <h2>Showing countries...</h2>
-    </>
-  )
+const ViewCountries = ({countryList, search,})=> {
+  if (search === ''){
+    return (
+      <>
+        <h2>Countries...</h2>
+        no data
+      </>
+    )
+  }
+  else {
+    const filteredCountry = countryList.filter(country => country['name']['common'].toLowerCase().includes(search))
+    return (
+      <>
+        <h2>Countries...</h2>
+        {
+          filteredCountry.map(country => <p key={country['name']['common']}>{country['name']['common']}</p>)
+        }
+      </>
+    )
+  }
 }
 
 export default App;
