24c53bb2642746b4b7b9469b80b01e9b059b85e4
Thato Lebethe <thatofreelance@gmail.com>
2023-06-14 14:05:41 +0200

    helper functions and unit tests, step 3

--- Part4/bloglistbackend/utils/list_helper.js
+++ Part4/bloglistbackend/utils/list_helper.js
@@ -9,8 +9,22 @@ const totalLikes = (blogs) => {
     }, 0)
 }
 
+const favoriteBlog = (blogs)=> {
+    let total = 0
+    let blog ;
+    for(let i =0 ; i < blogs.length; i++){
+        if(blogs[i].likes> total){
+            total = blogs[i].likes
+            blog = blogs[i]
+        }
+    }
+    const {author, title, likes} = blog
+    return {author, title, likes}
+}
+
 
 module.exports = {
     dummy,
     totalLikes,
+    favoriteBlog
 }
