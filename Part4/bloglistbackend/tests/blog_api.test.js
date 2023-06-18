b712e174d13b16f4d54819c601b81f27e7d923d3
Thato Lebethe <thatofreelance@gmail.com>
2023-06-18 18:23:07 +0200

    4.8-4.12 tests done

--- Part4/bloglistbackend/tests/blog_api.test.js
+++ Part4/bloglistbackend/tests/blog_api.test.js
@@ -49,6 +49,79 @@ test('content of first item equal to test2', async()=> {
     expect(content).toContain('cool beans')
 }, 10000)
 
+test('item in db contains an id', async()=> {
+    const res = await api.get('/api/blogs')
+    let blog = res.body[0]
+    expect(blog.id).toBeDefined()
+    blog = res.body[1]
+    expect(blog.id).toBeDefined()
+})
+
+test('check we can make a succesfull post', async ()=> {
+    await api.post('/api/blogs').send({
+        "title": "test3",
+        "author": "hello tests",
+        "url": "test url 1"
+    })
+    const res = await api.get('/api/blogs')
+    expect(res.body).toHaveLength(initialBlogs.length+1)
+    expect(res.body[2].id).toBeDefined()
+    const content = res.body.map(item => item.title)
+    expect(content).toContain('test3')
+})
+
+test('check that when i post default likes equal 0', async ()=> {
+    await api.post('/api/blogs').send({
+        "title": "test4",
+        "author": "max verstappen",
+        "url": "test url 1"
+    })
+    const res = await api.get('/api/blogs')
+    expect(res.body).toHaveLength(initialBlogs.length+1)
+    const content = res.body.map(item => item.title)
+    expect(content).toContain('test4')
+    expect(res.body[2].upvotes).toBeDefined()
+    expect(res.body[2].upvotes).toEqual(0)
+    
+})
+
+
+test('bad blog post missing url', async ()=> {
+    let res = await api.post('/api/blogs').send({
+        "title": "test4",
+        "author": "max verstappen",
+        // "url": "test url 1"
+    })
+    expect(res.body.error).toBeDefined()
+    expect(res.body.error).toEqual('Blog validation failed: url: Path `url` is required.')
+    expect(res.status).toEqual(400)
+})
+
+test('bad blog post missing title', async ()=> {
+    let res = await api.post('/api/blogs').send({
+        // "title": "test4",
+        "author": "max verstappen",
+        "url": "test url 1"
+    })
+    expect(res.body.error).toBeDefined()
+    expect(res.body.error).toEqual('Blog validation failed: title: Path `title` is required.')
+    expect(res.status).toEqual(400)
+})
+
+test('bad blog post missing author', async ()=> {
+    let res = await api.post('/api/blogs').send({
+        "title": "test4",
+        // "author": "max verstappen",
+        "url": "test url 1"
+    })
+    expect(res.body.error).toBeDefined()
+    expect(res.body.error).toEqual('Blog validation failed: author: Path `author` is required.')
+    expect(res.status).toEqual(400)
+})
+
+
+
+
 afterAll(async ()=> {
     await mongoose.connection.close()
 })
